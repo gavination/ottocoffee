1d047202c19b947b09a69db0eab5de34a4ea578d
timmyreilly <timreilly@live.com>
2015-09-15 15:50:20 -0700

    added stuff

--- otto-control.py
+++ otto-control.py
@@ -2,14 +2,49 @@ import RPi.GPIO as GPIO
 from flask import Flask, render_template, request
 app = Flask(__name__)
 
+from Tkinter import *
+import RPi.GPIO as GPIO
+import time
+
 GPIO.setmode(GPIO.BCM)
+GPIO.setup(18, GPIO.OUT)
+pwm = GPIO.PWM(18, 100)
+pwm.start(5)
 
-pins = {
-	24 : {'name': 'motor', 'state': GPIO.LOW},
-	25 : {'name': 'motor', 'state': GPIO.LOW}
-}
+class App:
 
-# Set each pin as an output and make it LOW
+    def __init__(self, master):
+        frame = Frame(master)
+        frame.pack()
+        scale = Scale(frame, from_=0, to=180,
+              orient=HORIZONTAL, command=self.update)
+        scale.grid(row=0)
 
-for pin in pins: 
-	GPIO.setup(pin, GPIO.OUT)
+
+    def update(self, angle):
+        duty = float(angle) / 10.0 + 2.5
+        pwm.ChangeDutyCycle(duty)
+
+
+
+root = Tk()
+root.wm_title('Servo Control')
+app = App(root)
+root.geometry("200x50+0+0")
+root.mainloop()
+
+
+@app.route("/")
+def main():
+    now = datetime.datetime.now()
+   timeString = now.strftime("%Y-%m-%d %H:%M")
+   templateData = {
+      'title' : 'HELLO!',
+      'time': timeString
+      }
+    root = Tk()
+    root.wm_title('Servo Control')
+    app = App(root)
+    root.geometry("200x50+0+0")
+    root.mainloop()
+    return render_template('main.html', **templateData)
