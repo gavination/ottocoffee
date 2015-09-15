82c4b5ba45c9653cc033aff158e6b2a1150a55dc
timmyreilly <timreilly@live.com>
2015-09-15 14:41:39 -0700

    better

--- servo.py
+++ servo.py
@@ -1,54 +1,29 @@
-import RPi.GPIO as GPIO
+# Servo Control
 import time
+def set(property, value):
+	try:
+		f = open("/sys/class/rpi-pwm/pwm0/" + property, 'w')
+		f.write(value)
+		f.close()	
+	except:
+		print("Error writing to: " + property + " value: " + value)
 
-GPIO.setmode(GPIO.BCM)
 
-enable_pin = 18
-coil_A_1_pin = 4
-coil_A_2_pin = 17
-coil_B_1_pin = 23
-coil_B_2_pin = 24
+def setServo(angle):
+	set("servo", str(angle))
+	
+		
+set("delayed", "0")
+set("mode", "servo")
+set("servo_max", "180")
+set("active", "1")
 
-GPIO.setup(enable_pin, GPIO.OUT)
-GPIO.setup(coil_A_1_pin, GPIO.OUT)
-GPIO.setup(coil_A_2_pin, GPIO.OUT)
-GPIO.setup(coil_B_1_pin, GPIO.OUT)
-GPIO.setup(coil_B_2_pin, GPIO.OUT)
-
-GPIO.output(enable_pin, 1)
-
-def forward(delay, steps):  
-  for i in range(0, steps):
-    setStep(1, 0, 1, 0)
-    time.sleep(delay)
-    setStep(0, 1, 1, 0)
-    time.sleep(delay)
-    setStep(0, 1, 0, 1)
-    time.sleep(delay)
-    setStep(1, 0, 0, 1)
-    time.sleep(delay)
-
-def backwards(delay, steps):  
-  for i in range(0, steps):
-    setStep(1, 0, 0, 1)
-    time.sleep(delay)
-    setStep(0, 1, 0, 1)
-    time.sleep(delay)
-    setStep(0, 1, 1, 0)
-    time.sleep(delay)
-    setStep(1, 0, 1, 0)
-    time.sleep(delay)
-
-  
-def setStep(w1, w2, w3, w4):
-  GPIO.output(coil_A_1_pin, w1)
-  GPIO.output(coil_A_2_pin, w2)
-  GPIO.output(coil_B_1_pin, w3)
-  GPIO.output(coil_B_2_pin, w4)
+delay_period = 0.01
 
 while True:
-  delay = raw_input("Delay between steps (milliseconds)?")
-  steps = raw_input("How many steps forward? ")
-  forward(int(delay) / 1000.0, int(steps))
-  steps = raw_input("How many steps backwards? ")
-  backwards(int(delay) / 1000.0, int(steps))
+	for angle in range(0, 180):
+		setServo(angle)
+		time.sleep(delay_period)
+	for angle in range(0, 180):
+		setServo(180 - angle)
+		time.sleep(delay_period)
